d24f3674c63e69798d1f6eebba15ea12da356aa5
Chris Dahms <zentrification@gmail.com>
2020-02-16 11:10:56 -0500

    added helper to generate image html

--- config.rb
+++ config.rb
@@ -32,12 +32,47 @@ page '/*.txt', layout: false
 # Helpers
 # Methods defined in the helpers block are available in templates
 # https://middlemanapp.com/basics/helper-methods/
+helpers do
 
-# helpers do
-#   def some_helper
-#     'Helping'
-#   end
-# end
+  # <% image '/path/to/image.jpg' %>
+  # <% image '/path/to/image.jpg', class: 'is-16by9' %>
+  # <% image '/path/to/image.jpg', hover: '/path/to/hover-image.jpg' %>
+  # <% image '/path/to/image.jpg', class: 'is-16by9', hover: '/path/to/hover-image.jpg' %>
+  #
+  # <% image '/path/to/image.jpg' do %>
+  #   <p>text that appears on hover</p>
+  # <% end %>
+  #
+  # <% image '/path/to/image.jpg', class: 'is-16by9' do %>
+  #   <p>text that appears on hover</p>
+  # <% end %>
+  def image(filename, options = {}, &block)
+    figure_classes = ['image']
+    image = image_tag(filename)
+
+    if options[:class]
+      figure_classes << options[:class]
+    end
+
+    if options[:hover]
+      hover_image = image_tag(options[:hover], class: 'hover-image')
+    end
+
+    if block_given?
+      hover_html = content_tag :div, class: 'hover-text' do
+        capture_html(&block)
+      end
+      figure_classes << 'has-hover-background'
+    end
+
+    content = content_tag :figure, class: figure_classes.join(' ') do
+      image + hover_image + hover_html
+    end
+
+    concat_content content
+  end
+
+end
 
 # Build-specific configuration
 # https://middlemanapp.com/advanced/configuration/#environment-specific-settings
